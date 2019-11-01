9fc47ab6cf34e8675a7fbfa0ecb45deec221a3cb
anhntth1807026 <anhntth1807026@fpt.edu.vn>
2019-11-01 09:56:27 +0700

    day giao dien goc admin

---
+++ src/components/Landing/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+// import * as ROUTES from "../constants/routes";
+// import {Link} from "react-router-dom";
+
+class AboutPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Thử nghiệm</h1>
+            </div>
+        )
+    }
+}
+
+export default AboutPage
